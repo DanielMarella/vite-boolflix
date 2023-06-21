b3399c5c7701aea95c9ef79942cb789c934bf24e
DanielMarella <marella.daniel96@gmail.com>
2023-06-21 15:23:44 +0200

    add function for search movies,  add axios

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,8 +1,10 @@
 <template>
     <div>
-        <h3>Sono il MAIN</h3>
+        <FindMovie />
     </div>
-    <FindMovie />
+    <div class="container mb-5">
+        <h1>Lista dei film:</h1>
+    </div>
     <MovieList />
 </template>
 
@@ -10,6 +12,8 @@
 <script>
 import MovieList from './MovieList.vue';
 import FindMovie from './FindMovie.vue';
+import axios  from 'axios';
+
 
     name : 'MainApp'
 export default {
@@ -17,8 +21,31 @@ export default {
     components : {
         MovieList,
         FindMovie,
-    }
+    },
     
+    data() {
+            return {
+                movies : [],
+            }
+        },
+
+        methods: {
+            searchfilm(){
+            axios.get('https://api.themoviedb.org/3/movie/550?api_key=6e246ef7707d5632049a8350bf230c0f')
+            .then( (response) => {
+                this.movies = response.data;
+                console.log(this.movies);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+        },
+    },
+
+    created() {
+        this.searchfilm();
+    },
+
 }
 </script>
 
