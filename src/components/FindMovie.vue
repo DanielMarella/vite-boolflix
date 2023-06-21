7f2ff0d09c946e9a96a3629f0b89e02877ca4aee
DanielMarella <marella.daniel96@gmail.com>
2023-06-21 15:23:07 +0200

    add a input text for research movies

--- src/components/FindMovie.vue
+++ src/components/FindMovie.vue
@@ -1,13 +1,20 @@
 <template>
+    <div class="container mb-5">
 
+        <input type="text">
+        <button> Clicca qui per ricercare</button>
+
+    </div>
 </template>
 
 
 <script>
     name : 'FindMovie'
-export default {
-    
+
+    export default {
+        
 }
+
 </script>
 
 <style lang="scss" scoped>
