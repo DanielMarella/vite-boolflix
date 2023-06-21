7d242c98ef93f8777a6569739f5479306e4c4af3
DanielMarella <marella.daniel96@gmail.com>
2023-06-21 12:09:34 +0200

    adding the component FindMovie to the main

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -2,19 +2,21 @@
     <div>
         <h3>Sono il MAIN</h3>
     </div>
+    <FindMovie />
     <MovieList />
 </template>
 
 
 <script>
 import MovieList from './MovieList.vue';
-
+import FindMovie from './FindMovie.vue';
 
     name : 'MainApp'
 export default {
 
     components : {
         MovieList,
+        FindMovie,
     }
     
 }
