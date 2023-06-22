9720b0d72c2674c3d69e22d3c533354fa7c2147a
DanielMarella <marella.daniel96@gmail.com>
2023-06-22 12:05:04 +0200

    adding two new component for visualize the film research, adding props

---
+++ src/components/SerieTvList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container d-flex flex-wrap">
+        <SingleFilm v-for="films in films"
+        :name = "films.name"
+        :originalName = "films.original_name"
+        :lang = "films.original_language"
+        :rate = "films.vote_average"
+
+        />
+    </div>
+</template>
+
+
+<script>
+import SingleFilm from './SingleFilm.vue';
+
+    name : 'SerieTvList'
+
+export default {
+
+    data() {
+        return {
+
+        }
+    },
+
+
+    components: {
+        SingleFilm,
+    },
+
+    props : {
+        films : Array,  
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
---
+++ src/components/SingleFilm.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="card mx-3 mb-3">
+        <h3>Tv Series</h3>
+        <p>Nome: {{name}}</p>
+        <p>Nome Originale: {{ originalName }}</p>
+        <p>Lingua: {{ lang }}</p>
+        <p>Voto: {{ rate }}</p>
+    </div>
+    
+    
+    </template>
+    
+    
+    <script>
+        name : 'SingleFilm'
+    export default {
+        props : {
+            name : String,
+            originalName : String,
+            lang : String,
+            rate : Number,
+        }
+    }
+    </script>
+    
+    <style lang="scss" scoped>
+        .card{
+            width: 200px ;
+            height: 250px ;
+            border: 2px solid black;
+        }
+    </style>
