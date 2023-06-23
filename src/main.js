4d9b481e5a5626951d66dc0f7205c244f0c085fc
DanielMarella <marella.daniel96@gmail.com>
2023-06-23 15:55:09 +0200

    import fas and far icon

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 library.add(faUserSecret)
+library.add(fas, far);
 createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
 .mount('#app')
